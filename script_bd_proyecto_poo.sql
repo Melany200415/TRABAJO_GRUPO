7ded0c5a76f4e3b8080b253d9878ea7c1ed2b781
Melany200415 <melany.perugachi@epn.edu.ec>
2026-07-22 02:04:45 -0500

    Add files via upload
    Scrip Base de Datos

---
+++ script_bd_proyecto_poo.sql
@@ -0,0 +1,11 @@
+use papeleria;
+
+show table status;
+select *from categorias;
+select * from detalle_venta;
+select * from movimientos_inventario;
+select * from productos;
+select * from roles;
+select * from usuarios; 
+select * from ventas;
+select *from proveedores;
